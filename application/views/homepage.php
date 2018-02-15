3ffffcbd474aef53189c1b0baf2c05512d0c0f3b
Tim Bruecker <brooker_113@hotmail.com>
2018-02-15 14:20:36 -0800

    Added job2

---
+++ application/views/homepage.php
@@ -0,0 +1,19 @@
+<div class="alert alert-info">{remaining_tasks} tasks are left to do!</div>
+
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Task</th>
+        <th>Priority</th>
+    </tr>
+
+    {display_tasks}
+	<tr>
+	    <td>{id}</td>
+	    <td>{task}</td>
+	    <td>{priority}</td>
+	</tr>
+	{/display_tasks}
+
+</table>
+
