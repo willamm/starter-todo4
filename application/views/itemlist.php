8cea0ca8fa0aaa384d2eb9cc4bb0e5c267d54b58
Tim Bruecker <brooker_113@hotmail.com>
2018-03-01 13:41:56 -0800

    Added pagination

--- application/views/itemlist.php
+++ application/views/itemlist.php
@@ -1,9 +1,9 @@
-<h3>Tasks by Category</h3>
+{pagination}
 <table class="table">
         <tr>
                 <th>Id</th>
                 <th>Task</th>
                 <th>Status</th>
         </tr>
-        {display_tasks} 
+        {display_tasks}
 </table>
