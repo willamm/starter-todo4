09de5e9ee6548ad79a5d3550d929a5215e0f1f39
William Murphy <will.murphy1994@gmail.com>
2018-03-01 14:05:09 -0800

    Add view and controller for roles

---
+++ application/controllers/Roles.php
@@ -0,0 +1,10 @@
+<?php
+
+class Roles extends Application
+{
+    public function actor($role = ROLE_GUEST)
+    {
+        $this->session->set_userdata('userrole', $role);
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+}
