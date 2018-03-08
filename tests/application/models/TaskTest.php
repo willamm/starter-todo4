87b7cbf86c5a966ae76d2c1b75e6029ae5631fd3
Tim Bruecker <brooker_113@hotmail.com>
2018-03-08 13:46:16 -0800

    Added unit tests

---
+++ tests/application/models/TaskTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+// Extend from the entity
+ class TaskTest extends TestCase
+  {
+    private $CI;
+    public function setUp()
+    {
+      // Load CI instance normally
+      $this->CI = &get_instance();
+    }
+    public function testGetPost()
+    {
+      $_SERVER['REQUEST_METHOD'] = 'GET';
+      $_GET['foo'] = 'bar';
+      $this->assertEquals('bar', $this->CI->input->get_post('foo'));
+    }
+  }
